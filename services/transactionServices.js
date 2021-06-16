3ebbdb144b9f71e43fdd15aaea3a94e119ea2518
Developer Catalyst <dev@catalyst.sh>
2021-06-16 18:38:23 +0530

    removed redundant code

--- services/transactionServices.js
+++ services/transactionServices.js
@@ -32,49 +32,7 @@ async function makeTransaction(to,from,amount,activityName,purpose)
 }
 
 
-async function userTransaction(req,res)
-{
-    console.log("request to make transaction recieved!!!!");
-    [to,from,amount,activityName,purpose]=[req.body.to,req.body.from,req.body.amount,req.body.activityName,req.body.purpose];
-  
-    try
-    {
-               
-        var _user = await User.findOne({userName:from});
-        if(_user.money<amount)
-        {
-            res.send("409")
-            return;
-        }
-        _user.money-=amount;
-        _user.save();
-        _user = await User.findOne({userName:to});
-        if(activityName=="")
-        {
-            _user.superAdminMoney+=amount;
-            _user.save();
-        }
-
-        else
-        {
-            _user.adminMoney+=amount;
-            _user.save();
-            console.log(activityName);
-            var _activity  = await Activity.findOne({name:activityName});
-            _activity.collections+=amount;
-            _activity.save();
-        }
-
-    const transaction = new Transaction({ to: to, from:from, amount:amount,activityName:activityName,purpose:purpose }); 
-    const result = await transaction.save();
-    console.log(result);
-    res.send(result);   }
-     catch(e){console.log("some error occured!!!",e);
-    res.send("409")}
-}
-
 module.exports.showUserTransactions = showUserTransactions;
 module.exports.showAll = showAll;
 module.exports.showAdminTransactions = showAdminTransactions;
 module.exports.makeTransaction = makeTransaction;
-module.exports.userTransaction = userTransaction;
