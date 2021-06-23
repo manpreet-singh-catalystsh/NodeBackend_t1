ace9d837f4e74b448c100b19d0d20996427f1d4b
Developer Catalyst <dev@catalyst.sh>
2021-06-23 12:11:16 +0530

    Password hashed ,    Response and error generalise ,    jwt  verified

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,23 +1,33 @@
 const activityService = require("../services/activityServices");
+const Response = require("../middleware/response");
+const Messages = require("../messages");
 
 async function showAll(req, res) {
   try {
-    res.send(await activityService.activityList());
-  } catch (e) {}
+    res.send(new Response( Messages.success.code,Messages.success.description,await activityService.activityList()));
+    //res.send(new Response( "","",await activityService.activityList()));
+  } catch (e) {
+    res.send(new Response( Messages.fail.code,Messages.fail.description,""));
+  }
 }
 
 async function showByCategory(req, res) {
   try {
-    res.send(await activityService.activityByCategory(req.params.category));
-  } catch (e) {}
+    res.send(new Response( Messages.success.code,Messages.success.description,await activityService.activityByCategory(req.params.category)));
+
+  } catch (e) {
+    res.send(new Response( Messages.fail.code,Messages.fail.description,""));
+  }
 }
 
 async function getUrl(req, res) {
   try {
-    res.send({
+    res.send(new Response( Messages.success.code,Messages.success.description,{
       Url: await activityService.activityUrl(req.params.activityName),
-    });
-  } catch (e) {}
+    }));
+  } catch (e) {
+    res.send(new Response( Messages.fail.code,Messages.fail.description,""));
+  }
 }
 
 module.exports = {
